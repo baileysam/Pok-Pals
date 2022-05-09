a7d3302692026670f1bd5c3e8a5640c53b73a899
baileysam190 <baileysam190@gmail.com>
2022-05-09 10:29:59 +0100

    Adding login page. Adding the navigation and burger menu.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+# Serve the app
+`python3 server/app.py` 
+
 # Compile Sass to CSS
 `sass --watch assets/scss/*.scss server/static/global.css`
--- server/app.py
+++ server/app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app  = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('pages/login.html')
+
+@app.route('/splash')
+def splash():
+    return render_template('pages/splash.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
